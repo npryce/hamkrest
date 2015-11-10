501055ee2fc0a371588f1599412c01b2c7ce8580
Nat Pryce <sw@natpryce.com>
2015-11-10 08:59:44 +0000

    Fix definition of assertThat

--- src/main/kotlin/org/hamcrest/kotlin/junit.kt
+++ src/main/kotlin/org/hamcrest/kotlin/junit.kt
@@ -1,7 +1,15 @@
 package org.hamcrest.kotlin
 
 
-public fun <T> assertThat(message: String? = null, actual: T, criteria: Matcher<T>) {
+public fun <T> assertThat(actual: T, criteria: Matcher<T>) {
+    _assertThat(null, actual, criteria)
+}
+
+public fun <T> assertThat(message: String, actual: T, criteria: Matcher<T>) {
+    _assertThat(message, actual, criteria)
+}
+
+private fun <T> _assertThat(message: String?, actual: T, criteria: Matcher<T>) {
     criteria(actual).let { judgement ->
         if (judgement is MatchResult.Mismatch) {
             throw AssertionError(
