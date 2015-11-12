ef0f7813c368153ed32feb20143ee3d7914c6678
Nat Pryce <sw@natpryce.com>
2015-11-12 00:09:24 +0000

    Improve legibility

--- src/main/kotlin/org/hamcrest/kotlin/core_matchers.kt
+++ src/main/kotlin/org/hamcrest/kotlin/core_matchers.kt
@@ -53,12 +53,12 @@ public inline fun <reified T : Any> cast(downcastMatcher: Matcher<T> ) : Matcher
     }
 }
 
-public fun <N :Comparable<N>> greaterThan(n:N) = _comparesAs(n, "greater than") {it > 0}
-public fun <N :Comparable<N>> greaterThanOrEqualTo(n:N) = _comparesAs(n, "greater than or equal to") {it >= 0}
-public fun <N :Comparable<N>> lessThan(n:N) = _comparesAs(n, "less than") {it < 0}
-public fun <N :Comparable<N>> lessThanOrEqualTo(n:N) = _comparesAs(n, "less than or equal to") {it <= 0}
+public fun <N :Comparable<N>> greaterThan(n:N) = _comparesAs("greater than", n) {it > 0}
+public fun <N :Comparable<N>> greaterThanOrEqualTo(n:N) = _comparesAs("greater than or equal to", n) {it >= 0}
+public fun <N :Comparable<N>> lessThan(n:N) = _comparesAs("less than", n) {it < 0}
+public fun <N :Comparable<N>> lessThanOrEqualTo(n:N) = _comparesAs("less than or equal to", n) {it <= 0}
 
-private fun <N :Comparable<N>> _comparesAs(n: N, description: String, expectedSignum: (Int)->Boolean) : Matcher<N> {
+private fun <N :Comparable<N>> _comparesAs(description: String, n: N, expectedSignum: (Int) -> Boolean) : Matcher<N> {
     return object : Matcher.Primitive<N>() {
         override fun invoke(actual: N): MatchResult =
                 match(expectedSignum(actual.compareTo(n))) {"was ${delimit(actual)}"}
