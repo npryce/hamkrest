6d45e76c7a9441dd11395534db1fadfef117bb96
Nat Pryce <sw@natpryce.com>
2015-11-13 14:36:10 +0000

    Delimit elements in iterables and ranges

--- src/main/kotlin/com/natpryce/hamkrest/delimit.kt
+++ src/main/kotlin/com/natpryce/hamkrest/delimit.kt
@@ -3,7 +3,9 @@ package com.natpryce.hamkrest
 fun delimit(v: Any?): String = when (v) {
     null -> "null"
     is String -> "\"" + v.replace("\\", "\\\\").replace("\"", "\\\"") + "\""
-    is Pair<*,*> -> Pair(delimit(v.first), delimit(v.second)).toString()
+    is Pair<*, *> -> Pair(delimit(v.first), delimit(v.second)).toString()
+    is Range<*> -> "${delimit(v.start)}..${delimit(v.end)}"
+    is Iterable<*> -> v.map(::delimit).joinToString(prefix = "[", separator= ", ", postfix = "]")
     else -> v.toString()
 }
 
--- src/test/kotlin/com/natpryce/hamkrest/utility_tests.kt
+++ src/test/kotlin/com/natpryce/hamkrest/utility_tests.kt
@@ -4,12 +4,23 @@ import org.junit.Test
 
 class DelimitingValuesInStrings {
     @Test
-    fun escapes_quotes_in_strings() {
+    fun quotes_around_string_values_and_escaped_within() {
         assertThat(delimit("hello, \"bob\""), equalTo("\"hello, \\\"bob\\\"\""))
     }
 
     @Test
-    fun delimits_elements_of_pairs() {
+    fun pairs() {
         assertThat(delimit(Pair("x", "y")), equalTo("(\"x\", \"y\")"))
+    }
+
+    @Test
+    fun iterables() {
+        assertThat(delimit(listOf(1,2)), equalTo("[1, 2]"))
+        assertThat(delimit(listOf("1","2")), equalTo("""["1", "2"]"""))
+    }
+
+    @Test
+    fun range() {
+        assertThat(delimit(1..8), equalTo("1..8"))
     }
 }
