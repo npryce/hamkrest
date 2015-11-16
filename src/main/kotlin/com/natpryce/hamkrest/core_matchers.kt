1e47c92d2b2134a6fcee6c98f153815ab7284053
Nat Pryce <sw@natpryce.com>
2015-11-16 00:15:26 +0000

    Some more documentation comments

--- src/main/kotlin/com/natpryce/hamkrest/core_matchers.kt
+++ src/main/kotlin/com/natpryce/hamkrest/core_matchers.kt
@@ -3,6 +3,9 @@ package com.natpryce.hamkrest
 import com.natpryce.hamkrest.internal.match
 
 
+/**
+ * Returns a matcher that reports if a value is equal to an [expected] value.
+ */
 fun <T> equalTo(expected: T): Matcher<T> =
         object : Matcher.Primitive<T>() {
             override fun invoke(actual: T): MatchResult = match(actual == expected) { "was ${describe(actual)}" }
@@ -11,12 +14,17 @@ fun <T> equalTo(expected: T): Matcher<T> =
         }
 
 
+/**
+ * Returns a matcher that reports if a value is null.
+ */
 fun <T> absent(): Matcher<T?> = object : Matcher.Primitive<T?>() {
     override fun invoke(actual: T?): MatchResult = match(actual == null) { "was ${describe(actual)}" }
     override fun description(): String = "null"
 }
 
-
+/**
+ * Returns a matcher that reports if a value is not null and meets the criteria of the [valueMatcher]
+ */
 fun <T> present(valueMatcher: Matcher<T>): Matcher<T?> =
         object : Matcher.Primitive<T?>() {
             override fun invoke(actual: T?): MatchResult {
@@ -31,6 +39,10 @@ fun <T> present(valueMatcher: Matcher<T>): Matcher<T?> =
             }
         }
 
+/**
+ * Returns a matcher that reports if a value of [Any] type is of a type compatible with [downcastMatcher] and, if so,
+ * if the value meets its criteria.
+ */
 inline fun <reified T : Any> cast(downcastMatcher: Matcher<T>): Matcher<Any> {
     return object : Matcher.Primitive<Any>() {
         override fun invoke(actual: Any): MatchResult {
@@ -50,9 +62,24 @@ inline fun <reified T : Any> cast(downcastMatcher: Matcher<T>): Matcher<Any> {
     }
 }
 
+/**
+ * Returns a matcher that reports if a [Comparable] value is greater than [n]
+ */
 fun <N : Comparable<N>> greaterThan(n: N) = _comparesAs("greater than", n) { it > 0 }
+
+/**
+ * Returns a matcher that reports if a [Comparable] value is greater than or equal to [n]
+ */
 fun <N : Comparable<N>> greaterThanOrEqualTo(n: N) = _comparesAs("greater than or equal to", n) { it >= 0 }
+
+/**
+ * Returns a matcher that reports if a [Comparable] value is less than [n]
+ */
 fun <N : Comparable<N>> lessThan(n: N) = _comparesAs("less than", n) { it < 0 }
+
+/**
+ * Returns a matcher that reports if a [Comparable] value is less than or equal to [n]
+ */
 fun <N : Comparable<N>> lessThanOrEqualTo(n: N) = _comparesAs("less than or equal to", n) { it <= 0 }
 
 private fun <N : Comparable<N>> _comparesAs(description: String, n: N, expectedSignum: (Int) -> Boolean): Matcher<N> {
@@ -66,6 +93,9 @@ private fun <N : Comparable<N>> _comparesAs(description: String, n: N, expectedS
     }
 }
 
+/**
+ * Returns a matcher that reports if a [Comparable] value falls within a given [Range]
+ */
 fun <T : Comparable<T>> isWithin(range: Range<T>) : Matcher<T> {
     fun _isWithin(actual: T, range: Range<T>): Boolean {
         return range.contains(actual)
