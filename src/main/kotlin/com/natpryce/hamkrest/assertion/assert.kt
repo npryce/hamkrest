0ce24e31795ee273d65779b450786c1e7e4ea1f1
Nat Pryce <sw@natpryce.com>
2016-01-05 07:56:18 +0000

    Overloads for assertThat given a binary function.  This lets you do, for example, assertThat(aCollection, ::contains, anElement) without having to define a matcher type

--- src/main/kotlin/com/natpryce/hamkrest/assertion/assert.kt
+++ src/main/kotlin/com/natpryce/hamkrest/assertion/assert.kt
@@ -6,6 +6,7 @@ import com.natpryce.hamkrest.MatchResult
 import com.natpryce.hamkrest.Matcher
 import com.natpryce.hamkrest.describe
 import kotlin.reflect.KFunction1
+import kotlin.reflect.KFunction2
 
 /**
  * Asserts that [criteria] matches [actual].
@@ -38,6 +39,22 @@ fun <T> assertThat(actual: T, criteria: KFunction1<T,Boolean>) {
  */
 fun <T> assertThat(message: String, actual: T, criteria: KFunction1<T,Boolean>) {
     _assertThat(message, actual, Matcher(criteria))
+}
+
+/**
+ * Asserts that [criteria]([actual], [other]) returns true.  On failure, the diagnostic is prefixed with [message].
+ * @throws AssertionError if there is a mismatch
+ */
+fun <T,U> assertThat(message: String, actual: T, criteria: KFunction2<T,U,Boolean>, other: U) {
+    _assertThat(message, actual, Matcher(criteria, other))
+}
+
+/**
+ * Asserts that [criteria]([actual], [other]) returns true.
+ * @throws AssertionError if there is a mismatch
+ */
+fun <T,U> assertThat(actual: T, criteria: KFunction2<T,U,Boolean>, other: U) {
+    _assertThat(null, actual, Matcher(criteria, other))
 }
 
 private fun <T> _assertThat(message: String?, actual: T, criteria: Matcher<T>) {
