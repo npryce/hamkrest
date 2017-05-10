a449f2ac8f1586b474ad353b7416bf530ff8e609
Andrey Zaytsev <a.einsam@gmail.com>
2017-05-10 18:35:49 +0200

    Added closeTo matcher for float/double

---
+++ src/test/kotlin/com/natpryce/hamkrest/number_matchers.kt
@@ -0,0 +1,27 @@
+package com.natpryce.hamkrest
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class CloseTo {
+    @Test
+    fun close() {
+        assertMatch(closeTo(3.0, 0.1)(3.01))
+        assertMatch(closeTo(3.0)(3.0))
+        assertMatch(closeTo(3.0f, 0.1f)(3.01f))
+        assertMatch(closeTo(3.0f)(3.0f))
+    }
+
+    @Test
+    fun not_close() {
+        assertMismatchWithDescription("a numeric value 4.0 differed by 0.9 more than error 0.1", closeTo(3.0, 0.1)(4.0))
+    }
+
+    @Test
+    fun description() {
+        assertEquals("is equal to 3.0 within 0.1", closeTo(3.0, 0.1).description)
+        assertEquals("is not equal to 3.0 within 0.01", closeTo(3.0, 0.01).negatedDescription)
+        assertEquals("is equal to 3.0 within 0.1", closeTo(3.0f, 0.1f).description)
+        assertEquals("is not equal to 3.0 within 0.01", closeTo(3.0f, 0.01f).negatedDescription)
+    }
+}
