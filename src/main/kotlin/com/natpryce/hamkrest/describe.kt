2c051c5fd4025754183187eb37e1a52c5b6299c6
Nat Pryce <sw@natpryce.com>
2017-10-17 16:02:25 +0100

    Address issue #34: don't describe arbitrary Iterables by iterating over their contents.
    This can cause stack overflow.
    
    Now only do so for Collection types.

--- src/main/kotlin/com/natpryce/hamkrest/describe.kt
+++ src/main/kotlin/com/natpryce/hamkrest/describe.kt
@@ -16,7 +16,7 @@ fun describe(v: Any?): String = when (v) {
     is Pair<*, *> -> Pair(describe(v.first), describe(v.second)).toString()
     is Triple<*, *, *> -> Triple(describe(v.first), describe(v.second), describe(v.third)).toString()
     is ClosedRange<*> -> "${describe(v.start)}..${describe(v.endInclusive)}"
-    is Iterable<*> -> v.map(::describe).joinToString(prefix = "[", separator = ", ", postfix = "]")
+    is Collection<*> -> v.map(::describe).joinToString(prefix = "[", separator = ", ", postfix = "]")
     is Map<*, *> -> v.entries.map { "${describe(it.key)}:${describe(it.value)}" }.joinToString(prefix = "{", separator = ", ", postfix = "}")
     else -> v.toString()
 }
--- src/test/kotlin/com/natpryce/hamkrest/describe_tests.kt
+++ src/test/kotlin/com/natpryce/hamkrest/describe_tests.kt
@@ -1,7 +1,9 @@
 package com.natpryce.hamkrest
 
 import com.natpryce.hamkrest.assertion.assert
+import com.natpryce.hamkrest.assertion.assertThat
 import org.junit.Test
+import java.nio.file.Paths
 
 class DelimitingValuesInStrings {
     @Test
@@ -42,5 +44,11 @@ class DelimitingValuesInStrings {
         }
 
         assert.that(describe(d), equalTo("d"))
+    }
+    
+    @Test
+    fun file_paths() {
+        val path = Paths.get("/foo/bar/baz")
+        assertThat(describe(path), equalTo("/foo/bar/baz"))
     }
 }
