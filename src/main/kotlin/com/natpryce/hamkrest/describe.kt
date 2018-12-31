b7659113cd10621b3ad20f7f701c80044be99305
Nat Pryce <sw@natpryce.com>
2018-12-31 00:45:52 +0000

    Deprecate the "should" API.

--- src/main/kotlin/com/natpryce/hamkrest/describe.kt
+++ src/main/kotlin/com/natpryce/hamkrest/describe.kt
@@ -47,4 +47,5 @@ interface SelfDescribing {
  * @property description the description of the value
  * @property value the value described by the description
  */
+@Deprecated("This is only used by the `should` package, which is deprecated. Use assertThat instead")
 class Described<T>(override val description: String, val value: T) : SelfDescribing
--- src/main/kotlin/com/natpryce/hamkrest/should/should.kt
+++ src/main/kotlin/com/natpryce/hamkrest/should/should.kt
@@ -4,6 +4,7 @@ package com.natpryce.hamkrest.should
 
 import com.natpryce.hamkrest.Described
 import com.natpryce.hamkrest.Matcher
+import com.natpryce.hamkrest.SelfDescribing
 import com.natpryce.hamkrest.assertion.assertThat
 import kotlin.reflect.KFunction1
 
@@ -11,6 +12,7 @@ import kotlin.reflect.KFunction1
 /**
  * Syntactic sugar for [assertThat] that can be used as an infix function
  */
+@Deprecated("use assertThat instead")
 infix fun <T> T.shouldMatch(matcher: Matcher<T>) {
     assertThat(this, matcher)
 }
@@ -18,6 +20,7 @@ infix fun <T> T.shouldMatch(matcher: Matcher<T>) {
 /**
  * Syntactic sugar for [assertThat] that can be used as an infix function
  */
+@Deprecated("use assertThat instead")
 infix fun <T> T.shouldMatch(f: KFunction1<T, Boolean>) {
     this shouldMatch Matcher(f)
 }
@@ -25,6 +28,7 @@ infix fun <T> T.shouldMatch(f: KFunction1<T, Boolean>) {
 /**
  * Syntactic sugar for a negated [assertThat] that can be used as an infix function
  */
+@Deprecated("use assertThat instead")
 infix fun <T> T.shouldNotMatch(matcher: Matcher<T>) {
     assertThat(this, !matcher)
 }
@@ -32,6 +36,7 @@ infix fun <T> T.shouldNotMatch(matcher: Matcher<T>) {
 /**
  * Syntactic sugar for a negated [assertThat] that can be used as an infix function
  */
+@Deprecated("use assertThat instead")
 infix fun <T> T.shouldNotMatch(f: KFunction1<T, Boolean>) {
     this shouldNotMatch Matcher(f)
 }
@@ -42,14 +47,17 @@ infix fun <T> T.shouldNotMatch(f: KFunction1<T, Boolean>) {
  * Used as:
  *         x describedAs "x" shouldMatch equalTo(10)
  */
+@Deprecated("use assertThat instead")
 infix fun <T> T.describedAs(name: String) = Described(name, this)
 
 /**
  * Syntactic sugar for [assertThat] that can be used as an infix function.
  */
+@Deprecated("use assertThat instead")
 infix fun <T> Described<T>.shouldMatch(criteria: Matcher<T>) = assertThat(this.description, this.value, criteria)
 
 /**
  * Syntactic sugar for [assertThat] that can be used as an infix function.
  */
+@Deprecated("use assertThat instead")
 infix fun <T> Described<T>.shouldMatch(p: KFunction1<T, Boolean>) = assertThat(this.description, this.value, Matcher(p))
