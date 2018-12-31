bcb62683e67cfb401f81d734ecf072db9e398dde
Nat Pryce <sw@natpryce.com>
2018-12-31 01:06:51 +0000

    Test for extending the describe function with a JVM service

--- src/test/kotlin/com/natpryce/hamkrest/describe_tests.kt
+++ src/test/kotlin/com/natpryce/hamkrest/describe_tests.kt
@@ -2,6 +2,7 @@ package com.natpryce.hamkrest
 
 import com.natpryce.hamkrest.assertion.assertThat
 import org.junit.Test
+import sun.awt.SunHints.Value
 import java.nio.file.Paths
 
 class DelimitingValuesInStrings {
@@ -9,39 +10,39 @@ class DelimitingValuesInStrings {
     fun quotes_around_string_values_and_escaped_within() {
         assertThat(describe("hello, \"bob\""), equalTo("\"hello, \\\"bob\\\"\""))
     }
-
+    
     @Test
     fun pairs() {
         assertThat(describe(Pair("x", "y")), equalTo("(\"x\", \"y\")"))
     }
-
+    
     @Test
     fun triples() {
         assertThat(describe(Triple("x", "y", "z")), equalTo("(\"x\", \"y\", \"z\")"))
     }
-
+    
     @Test
     fun iterables() {
         assertThat(describe(listOf(1, 2)), equalTo("[1, 2]"))
         assertThat(describe(listOf("1", "2")), equalTo("""["1", "2"]"""))
     }
-
+    
     @Test
     fun ranges() {
         assertThat(describe(1..8), equalTo("1..8"))
     }
-
+    
     @Test
     fun maps() {
         assertThat(describe(mapOf("a" to 1, "b" to 2)), equalTo("""{"a":1, "b":2}"""))
     }
-
+    
     @Test
-    fun describable_objects() {
+    fun self_describing_objects() {
         val d = object : SelfDescribing {
             override val description = "d"
         }
-
+        
         assertThat(describe(d), equalTo("d"))
     }
     
@@ -50,4 +51,22 @@ class DelimitingValuesInStrings {
         val path = Paths.get("/foo/bar/baz")
         assertThat(describe(path), equalTo("/foo/bar/baz"))
     }
+}
+
+
+class NeedsCustomDescription
+
+class DescribeExtensionPointTests {
+    @Test
+    fun can_extend_describe_function_with_jvm_services() {
+        assertThat(describe(NeedsCustomDescription()), equalTo("the-custom-description"))
+    }
+}
+
+class ExampleDescribeExtension : ValueDescription {
+    override fun describe(v: Any?) =
+        when (v) {
+            is NeedsCustomDescription -> "the-custom-description"
+            else -> null
+        }
 }
