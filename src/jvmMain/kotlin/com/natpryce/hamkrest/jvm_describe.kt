2d23093f7d82f5360ed44fc9dc754de80d371bc5
daviddenton-teal <61234025+daviddenton-teal@users.noreply.github.com>
2020-02-19 12:49:33 +0000

    Synchronise to Fix #59

--- src/jvmMain/kotlin/com/natpryce/hamkrest/jvm_describe.kt
+++ src/jvmMain/kotlin/com/natpryce/hamkrest/jvm_describe.kt
@@ -3,7 +3,7 @@ package com.natpryce.hamkrest
 import java.util.ServiceLoader
 import kotlin.reflect.KClass
 
-private val descriptionServices = ServiceLoader.load(ValueDescription::class.java)
+private val descriptionServices = synchronized(ValueDescription::class) { ServiceLoader.load(ValueDescription::class.java) }
 
 actual fun describe(v: Any?): String =
     descriptionServices.map { it.describe(v) }.filterNotNull().firstOrNull() ?: defaultDescription(v)
