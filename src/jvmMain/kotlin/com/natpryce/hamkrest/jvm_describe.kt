63c8e84dd1f21ed1de737a3e42162f3f6973a313
Nat Pryce <sw@natpryce.com>
2020-03-12 23:20:51 +0000

    Fix the fix for the concurrency error in `describe`.

--- src/jvmMain/kotlin/com/natpryce/hamkrest/jvm_describe.kt
+++ src/jvmMain/kotlin/com/natpryce/hamkrest/jvm_describe.kt
@@ -3,11 +3,9 @@ package com.natpryce.hamkrest
 import java.util.ServiceLoader
 import kotlin.reflect.KClass
 
-private val mutex = Any()
-private val descriptionServices = synchronized(mutex) { ServiceLoader.load(ValueDescription::class.java) }
-
 actual fun describe(v: Any?): String =
-    descriptionServices.map { it.describe(v) }.filterNotNull().firstOrNull() ?: defaultDescription(v)
-
+    ServiceLoader.load(ValueDescription::class.java).asSequence()
+        .mapNotNull { it.describe(v) }
+        .firstOrNull() ?: defaultDescription(v)
 
 actual val KClass<*>.reportedName : String get() = qualifiedName ?: "<unknown type>"
